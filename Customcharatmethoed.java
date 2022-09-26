36ad5185c05249fb95340ad17470c3037a823d3a
Girishyb <114143838+Girishyb@users.noreply.github.com>
2022-09-26 14:32:25 +0530

    Add files via upload

---
+++ Customcharatmethoed.java
@@ -0,0 +1,14 @@
+package com.string.mylength;
+
+public class Customcharatmethoed 
+{
+
+	public static char myCharAt(int ind,String s)
+	{
+		char ar[]=s.toCharArray();
+		
+		return ar[ind] ;
+	}
+	
+	
+}
